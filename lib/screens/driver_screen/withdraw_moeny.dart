40ead469e78f38dd82669b1a68a05959c9c694ae
Saurav <deveneoxys@gmail.com>
2023-04-19 11:38:07 +0530

    saurav 19.04.2023

--- lib/screens/driver_screen/withdraw_moeny.dart
+++ lib/screens/driver_screen/withdraw_moeny.dart
@@ -88,11 +88,15 @@ class _DriverWithdrawMoneyState extends State<DriverWithdrawMoney> {
                                       ),
                                     ],
                                   ),
-                                  Image(
-                                    image: AssetImage(AppAssets.walletIcon),
-                                    height: AddSize.size30,
-                                    width: AddSize.size50,
-                                  ),
+                                  Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                      color: AppTheme.primaryColor,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Icon(Icons.account_balance_wallet_sharp,color: Colors.white,),
+                                  )
                                 ],
                               ),
                             )),
